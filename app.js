3b2b6f46d11a419665ec488f25fcc5905facce0e
droy312 <dhritiman.roy312@gmail.com>
2023-06-16 12:26:53 +0530

    feat: Added .env file.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+
+/// Configuration for using .env file
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server is running in PORT ${PORT}`));
